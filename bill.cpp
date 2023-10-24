7c42b26ebc7258702417c8a6441c2f83cc7bc85d
Tharunish17 <143690349+Tharunish17@users.noreply.github.com>
2023-10-24 16:56:25 +0530

    bill.cpp

--- bill.cpp
+++ bill.cpp
@@ -5,10 +5,10 @@ int main()
 	printf("Enter your power usage");
 	scanf("%f",&a);
 	if (a>=0 && a<100)
-	 printf("Your bill is %f",2*a);
+	 printf("Your bill is %f",(2*a));
 	else if (a>=100 && a<200)
 	  printf("Your bill is %f",5*a);
 	else 
-	   printf("Your bill is %f",10*a);
+	   printf("Your bill is %f",(10*a)+(a/10));
     return 0;
 }
