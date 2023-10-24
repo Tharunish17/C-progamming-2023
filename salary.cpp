396f8c4e2007d2dc09e73930d030d221c3afcbc0
Tharunish17 <143690349+Tharunish17@users.noreply.github.com>
2023-10-24 16:35:35 +0530

    salary.cpp

--- salary.cpp
+++ salary.cpp
@@ -6,20 +6,22 @@ int main()
   scanf("%d",&a);
    printf("Enter your tier");
    scanf("%d",&tier);
-   if (1)
+   if (tier==1)
    {
    	b=(a*(17+25)/100)+a;
    	printf("Your salary is %d",b);
    }
-   else if (2)
+   else if (tier==2)
     {
      b=(a*(17+18)/100)+a;
 	 printf("Your salary is %d",b);
 	}
-	else 
+	else if (tier==3)
 	{
 	 b=(a*(17+12)/100)+a;
 	 printf("Your salary is %d",b);
 	}
+	else 
+	printf("INVALID");
     return 0;
 }
