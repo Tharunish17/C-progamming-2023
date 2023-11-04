c262805c571253b7a368dc98ec0c42de61cfddeb
Tharunish17 <143690349+Tharunish17@users.noreply.github.com>
2023-11-04 14:38:45 +0530

    Add files via upload

---
+++ float and integer.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c;
+	printf("Enter two values for multiplication");
+	scanf("%f %f",&a,&b);
+	c=a*b;
+	printf("%f\n",c);
+	int d=c;
+	printf("%d",d);
+	return 0;
+}
