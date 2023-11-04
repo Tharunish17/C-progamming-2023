e218d7fe70d76cb8627896eee76171e270805bc0
Tharunish17 <143690349+Tharunish17@users.noreply.github.com>
2023-11-04 15:22:13 +0530

    Add files via upload

---
+++ greatest.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter three digits");
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b;a>c)
+	printf("%d is the greatest",a);
+  else 	if(b>c)
+	printf("%d is the greatest",b);
+	else
+	  printf("%d is the greatest",c);
+	  return 0;
+}
